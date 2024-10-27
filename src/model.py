79e157eaa2d2b1dda7c3b853eaa4405f98d142f5
thomasport <thomasportugal5@gmail.com>
2024-10-27 19:47:06 -0300

    update support scripts

--- src/model.py
+++ src/model.py
@@ -1,5 +1,12 @@
 from sklearn.linear_model import lasso_path, LogisticRegression
-from sklearn.metrics import f1_score, recall_score, precision_score, confusion_matrix, roc_auc_score,RocCurveDisplay
+from sklearn.metrics import (
+    f1_score,
+    recall_score,
+    precision_score,
+    confusion_matrix,
+    roc_auc_score,
+    RocCurveDisplay,
+)
 from itertools import cycle
 import pandas as pd
 import seaborn as sns
@@ -7,28 +14,43 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-SEED = 42 
+SEED = 42
 
-def undersampling(df, prop=None,target='Class'):
-    c_min = df['Class'].value_counts().argmin()
-    
+
+def undersampling(df: pd.DataFrame, 
+                  prop=None, 
+                  target="Class") -> pd.DataFrame:
+    """Undersampling the majority class sample by a specific proportion. 
+       If the proportion is not informed, returns majority class with 
+       same proportion as minority class 
+
+
+    Args:
+        df (pd.DataFrame): Data with imabalanced target
+        prop (float, optional): Proportion to undersample. Defaults to None.
+        target (str, optional): Column name of target. Defaults to "Class".
+
+    Returns:
+        pd.DataFrame: Undersampled dataframe
+    """
+    c_min = df["Class"].value_counts().argmin()
+
     if not prop:
-        n_class_min = df['Class'].value_counts().min()
-        prop = n_class_min/df.shape[0]
+        n_class_min = df["Class"].value_counts().min()
+        prop = n_class_min / df.shape[0]
 
-    df_min = df[df[target]==c_min]
-    df_max = df[df[target]!=c_min]
+    df_min = df[df[target] == c_min]
+    df_max = df[df[target] != c_min]
 
-    n=int(prop*df.shape[0])
+    n = int(prop * df.shape[0])
     df_max_resampled = df_max.sample(n, random_state=SEED)
-    
-    return pd.concat([df_max_resampled,df_min])
+
+    return pd.concat([df_max_resampled, df_min])
 
 
-def calc_metrics(data: pd.DataFrame, 
-                 y_true='y_true', 
-                 y_pred='y_pred',
-                 scores='scores' )-> pd.DataFrame:
+def calc_metrics(
+    data: pd.DataFrame, y_true="y_true", y_pred="y_pred", scores="scores"
+) -> pd.DataFrame:
     """Calculate metrics from predictions
 
     Args:
@@ -38,30 +60,31 @@ def calc_metrics(data: pd.DataFrame,
         scores (str, optional): Column name of predicted scores. Defaults to 'scores'.
 
     Returns:
-        pd.DataFrame: _description_
+        pd.DataFrame: Table with performance metrics
     """
-    recall = recall_score(data[y_true],data[y_pred])
-    precision = precision_score(data[y_true],data[y_pred])
+    recall = recall_score(data[y_true], data[y_pred])
+    precision = precision_score(data[y_true], data[y_pred])
 
-    f1 = f1_score(data[y_true],data[y_pred])
+    f1 = f1_score(data[y_true], data[y_pred])
 
-    auc = roc_auc_score(data[y_true],data[scores])
+    auc = roc_auc_score(data[y_true], data[scores])
 
     money_cost = calc_cost(data)
 
-    
+    df_metrics = pd.DataFrame(
+        {
+            "recall": {"value": recall},
+            "precision": {"value": precision},
+            "f1": {"value": f1},
+            "auc": {"value": auc},
+            "Cost": {"value": money_cost},
+        }
+    ).style.format("{:.2f}")
 
-    df_metrics = pd.DataFrame({'recall': {'value':recall},
-                               'precision': {'value':precision},
-                               'f1': {'value':f1},
-                               'auc': {'value':auc},
-                               'Cost': {'value':money_cost}}).style.format('{:.2f}')
-    
     return df_metrics
 
-def cost(df: pd.DataFrame,
-         y_true='y_true', 
-         y_pred='y_pred'):
+
+def cost(df: pd.DataFrame, y_true="y_true", y_pred="y_pred"):
     """Calculate the custom cost from predictions
 
     Args:
@@ -75,24 +98,23 @@ def cost(df: pd.DataFrame,
     Returns:
         float: Cost for specific sample
     """
-    if (df[y_pred]==0) & (df[y_true]==0):
+    if (df[y_pred] == 0) & (df[y_true] == 0):
         return 0
-    elif (df[y_pred]==0) & (df[y_true]==1):
-        return -df['Amount']
-    elif (df[y_pred]==1) & (df[y_true]==0):
+    elif (df[y_pred] == 0) & (df[y_true] == 1):
+        return -df["Amount"]
+    elif (df[y_pred] == 1) & (df[y_true] == 0):
         return -2
-    elif (df[y_pred]==1) & (df[y_true]==1):
-        return 0.2*df["Amount"]
+    elif (df[y_pred] == 1) & (df[y_true] == 1):
+        return 0.2 * df["Amount"]
     else:
         raise ValueError
 
-def calc_cost(data: pd.DataFrame,
-              y_true='y_true', 
-              y_pred='y_pred')-> pd.Series:
+
+def calc_cost(data: pd.DataFrame, y_true="y_true", y_pred="y_pred") -> pd.Series:
     """_summary_
 
     Args:
-        data (pd.DataFrame): Sample with prediction, true label and amount 
+        data (pd.DataFrame): Sample with prediction, true label and amount
         y_true (str, optional): Column name of true label. Defaults to 'y_true'.
         y_pred (str, optional): Column name of predictions. Defaults to 'y_pred'.
 
@@ -100,14 +122,12 @@ def calc_cost(data: pd.DataFrame,
         pd.Series: Cost calculated for the sample
     """
 
-    costs_vector = data.apply(lambda x: cost(x,
-                                             y_true=y_true,
-                                             y_pred=y_pred),axis=1)
+    costs_vector = data.apply(lambda x: cost(x, y_true=y_true, y_pred=y_pred), axis=1)
     return costs_vector.sum()
-    
+
 
 def roc_auc_curve(y_true: pd.Series, scores: pd.Series):
-    """ Generate ROC curve from predictions
+    """Generate ROC curve from predictions
 
     Args:
         y_true (pd.Series): Series with true label
@@ -119,8 +139,8 @@ def roc_auc_curve(y_true: pd.Series, scores: pd.Series):
     return RocCurveDisplay.from_predictions(y_true, scores)
 
 
-def conf_mat(y_true : pd.Series, y_pred : pd.Series, figsize=(5,5)): 
-    """Generate Confusion Matrix 
+def conf_mat(y_true: pd.Series, y_pred: pd.Series, figsize=(5, 5)):
+    """Generate Confusion Matrix
 
     Args:
         y_true (pd.Series): True Label
@@ -134,18 +154,18 @@ def conf_mat(y_true : pd.Series, y_pred : pd.Series, figsize=(5,5)):
     conf_mat = confusion_matrix(y_true, y_pred)
     cm = pd.DataFrame(conf_mat, index=np.unique(y_true), columns=np.unique(y_true))
 
-    group_counts = ["{0:0.0f}".format(value) for value in
-                conf_mat.flatten()]
-    
-    group_percentages = ["{0:.2%}".format(value) for value in
-                        (conf_mat.T/np.sum(conf_mat,axis=1)).T.flatten()]
-    
-    labels = [f"{c}\n{perc}" for c, perc in
-            zip(group_counts,group_percentages)]
-    
-    labels = np.asarray(labels).reshape(2,2)
-    cm.index.name = 'True'
-    cm.columns.name = 'Predicted'
-    _ , ax = plt.subplots(figsize=figsize)
-    
-    return sns.heatmap(cm, cmap= "YlGnBu",annot=labels,fmt='',  ax=ax)
+    group_counts = ["{0:0.0f}".format(value) for value in conf_mat.flatten()]
+
+    group_percentages = [
+        "{0:.2%}".format(value)
+        for value in (conf_mat.T / np.sum(conf_mat, axis=1)).T.flatten()
+    ]
+
+    labels = [f"{c}\n{perc}" for c, perc in zip(group_counts, group_percentages)]
+
+    labels = np.asarray(labels).reshape(2, 2)
+    cm.index.name = "True"
+    cm.columns.name = "Predicted"
+    _, ax = plt.subplots(figsize=figsize)
+
+    return sns.heatmap(cm, cmap="YlGnBu", annot=labels, fmt="", ax=ax)
--- src/report.py
+++ src/report.py
@@ -1,28 +1,16 @@
 import numpy as np
-import pandas as pd 
+import pandas as pd
 from matplotlib import pyplot as plt
 import seaborn as sns
 from sklearn.metrics import confusion_matrix
 from pandas.io.formats.style import Styler
 from IPython.display import display_html
-from itertools import chain,cycle
+from itertools import chain, cycle
 
 
-def display_side_by_side(*args,titles=cycle([''])):
-    html_str=''
-    for df,title in zip(args, chain(titles,cycle(['</br>'])) ):
-        html_str+='<th style="text-align:center"><td style="vertical-align:top">'
-        html_str+=f'<h2 style="text-align: center;">{title}</h2>'
-        html_str+=df.render().replace('table','table style="display:inline"')
-        html_str+='</td></th>'
-    display_html(html_str,raw=True)
-  
-
-
-def plot_time_stab(data: pd.DataFrame, 
-                   line_y_col='bad', 
-                   bar_y_col = 'count', 
-                   time_col='time_hr') -> None :
+def plot_time_stab(
+    data: pd.DataFrame, line_y_col="bad", bar_y_col="count", time_col="time_hr"
+) -> None:
     """Generate plot with temporal characteristics
 
     Args:
@@ -32,20 +20,17 @@ def plot_time_stab(data: pd.DataFrame,
         time_col (str, optional): Column name of time variable. Defaults to 'time_hr'.
     """
 
-
-    fig, ax1 = plt.subplots(figsize=(18,6))
+    fig, ax1 = plt.subplots(figsize=(18, 6))
 
     fig.suptitle("Time Stability")
 
-    sns.lineplot(data = data[line_y_col], marker='o', sort = False, ax=ax1)
+    sns.lineplot(data=data[line_y_col], marker="o", sort=False, ax=ax1)
     ax2 = ax1.twinx()
 
-    sns.barplot(data = data, x=time_col, y=bar_y_col, alpha=0.5, ax=ax2)
+    sns.barplot(data=data, x=time_col, y=bar_y_col, alpha=0.5, ax=ax2)
 
 
-def bin_var(data: pd.DataFrame, 
-             col: str, 
-             n_bins=10) -> Styler:
+def bin_var(data: pd.DataFrame, col: str, n_bins=10) -> Styler:
     """Generate summary table of varible binned wit event rate and event ditribution for each bin
 
     Args:
@@ -57,15 +42,17 @@ def bin_var(data: pd.DataFrame,
         Styler: _description_
     """
     df = data.copy()
-    col_binned = col +'_binned'
+    col_binned = col + "_binned"
     cmap = sns.light_palette("red", as_cmap=True)
-    df[col_binned] = pd.qcut(df[col],q=n_bins)
-    df_agg = df.groupby(col_binned, observed=False).agg(
-                              count=(col,'count'),
-                              bad=('Class','sum')
-                              ).reset_index()
-    df_agg['bad_rate'] = df_agg.bad/df_agg['count']
-    df_agg['bad_distr'] = df_agg.bad/df_agg.bad.sum()
-    view  = df_agg.style.background_gradient(subset=['bad_rate','bad_distr'],cmap=cmap,vmax=1)
+    df[col_binned] = pd.qcut(df[col], q=n_bins)
+    df_agg = (
+        df.groupby(col_binned, observed=False)
+        .agg(count=(col, "count"), bad=("Class", "sum"))
+        .reset_index()
+    )
+    df_agg["bad_rate"] = df_agg.bad / df_agg["count"]
+    df_agg["bad_distr"] = df_agg.bad / df_agg.bad.sum()
+    view = df_agg.style.background_gradient(
+        subset=["bad_rate", "bad_distr"], cmap=cmap, vmax=1
+    )
     return view
-
